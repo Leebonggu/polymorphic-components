f3ea9e1e84e003dda50398e7731129058901068c
Leebonggu <leebonggu12@hanmail.net>
2022-04-18 21:32:17 +0900

    polymorphic

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,15 +2,18 @@ import type { ElementType, ComponentPropsWithoutRef, PropsWithChildren } from 'r
 
 type Rainbow = 'red' | 'oragne' | 'yellow' | 'green' | 'blue' | 'indigo' | 'violet';
 
-type TextProps<C extends ElementType> = {
-  as?: C;
-  color?: Rainbow | 'black'; 
+type AsProp<C extends ElementType> = {
+  as?: C
 }
 
 // React.PropsWithChidlren<{}>
-type Props<C extends ElementType> = PropsWithChildren<TextProps<C>> & Omit<ComponentPropsWithoutRef<C>, keyof TextProps<C>>
+type PropsToOmit<C extends ElementType, P>=  keyof (AsProp<C> & P)
 
-export function Text<C extends ElementType = 'span'>({ as, color, style, children, ...restProps }: Props<C>) {
+type PolymorphicComponentProps<C extends ElementType, Props = {}> = PropsWithChildren<Props & AsProp<C>> & Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>
+
+type TextProps = { color?: Rainbow | 'black' }
+
+export function Text<C extends ElementType = 'span'>({ as, color, style, children, ...restProps }: PolymorphicComponentProps<C, TextProps>) {
   const Component = as || 'span';
 
   const internalStyles = color ? { style: { ...style, color }} : {}
